ffebcf7e4d46417c776d5c962686c1f723d33f1c
FightFastest <17612135643@163.com>
2022-03-22 11:26:58 +0800

    自定义 vite config

---
+++ plugins/vite-test1-plugin.js
@@ -0,0 +1,46 @@
+// 手写vite 的plugin
+export default function (enforce) {
+    return {
+        config (userConfig, env) {
+            // 把当前的config 和 用户的配置合并之后，返回
+            // userConfig 原始用户配置
+            // env:一个描述配置环境的变量，包含正在使用的 mode 和 command
+            return {
+                resolve: {
+                    alias: {
+                        '@view': '/src/view'
+                    }
+                }
+            }
+        },
+        configResolved (config) {
+            // 它会拿到 上面 config 运行完成之后 的config
+            console.log(config.resolve)
+        },
+        configureServer (server) {
+            // 模块管理相关的内容， 是用于配置开发服务器的钩子
+            //  server 中包含所有模块的信息
+            server.middlewares.use((req, res, next) => {
+                // 自定义请求处理...
+                if (req.url === '/test') {
+                    res.end('configureServer  transform test')
+                } else {
+                    next()
+                }
+            })
+        },
+        transformIndexHtml (html) {
+            // 对输入的html 做处理
+            // html 是index.html 的内容
+            return html.replace('<div id="app"></div>', '<div id="app" class="6666"></div>')
+        },
+        handleHotUpdate (ctx) {
+            // 热更新是时候会触发这个选项
+            ctx.server.ws.send({
+                type: 'custom',
+                event: 'test',
+                data: { name: '2222' }
+            })
+        }
+    }
+}
